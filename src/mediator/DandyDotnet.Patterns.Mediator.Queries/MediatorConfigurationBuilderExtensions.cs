2065fbd5e9a5e2b620bb412e71368ffd9d1a0149
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-12 18:15:18 +0200

    queries plugin overhaul

---
+++ src/mediator/DandyDotnet.Patterns.Mediator.Queries/MediatorConfigurationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DandyDotnet.Patterns.Mediator.Configuration;
+using DandyDotnet.Patterns.Mediator.Queries.Abstractions;
+
+namespace DandyDotnet.Patterns.Mediator.Queries;
+
+/// <summary>
+/// Contains extensions for <see cref="MediatorConfigurationBuilder"/>.
+/// </summary>
+public static class MediatorConfigurationBuilderExtensions
+{
+    /// <summary>
+    /// Adds validation to the mediator.
+    /// </summary>
+    /// <param name="builder">The mediator configuration builder to add validation to.</param>
+    /// <returns>The mediator configuration builder.</returns>
+    public static MediatorConfigurationBuilder UseQueries(this MediatorConfigurationBuilder builder)
+    {
+        builder.UsePlugin(new MediatorQueriesConfiguration());
+        builder.ScanForServiceType(typeof(IQueryHandler<,>));
+
+        return builder;
+    }
+}
---
+++ src/mediator/DandyDotnet.Patterns.Mediator.Queries/MediatorQueriesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DandyDotnet.Patterns.Mediator.Abstractions.Requests.Mapping;
+using DandyDotnet.Patterns.Mediator.Configuration;
+using DandyDotnet.Patterns.Mediator.Queries.Abstractions;
+using DandyDotnet.Patterns.Mediator.Requests.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DandyDotnet.Patterns.Mediator.Queries;
+
+internal sealed class MediatorQueriesConfiguration : MediatorPluginConfiguration
+{
+    public override string Slot => MediatorConstants.Plugins.Queries.Slot;
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IRequestResponseMap>(_ => new RequestResponseMap(typeof(IQueryResponse<>), typeof(QueryResponse<>)));
+    }
+}
