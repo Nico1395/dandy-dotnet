1a8517c89ef4643bd3201410a33478f72a0f5956
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-12 13:47:55 +0200

    encoding implementations

---
+++ src/encoding/DandyDotnet.Encoding/Codes/AsciiPayloadEncoder.cs
@@ -0,0 +1,27 @@
+namespace DandyDotnet.Encoding.Codes;
+
+/// <summary>
+/// Encodes payloads using ASCII.
+/// </summary>
+public class AsciiEncoder : IEncoder
+{
+    /// <summary>
+    /// Encodes a string as ASCII bytes.
+    /// </summary>
+    /// <param name="payload">The payload to encode.</param>
+    /// <returns>The encoded <paramref name="payload"/>.</returns>
+    public ReadOnlyMemory<byte> Encode(string payload)
+    {
+        return System.Text.Encoding.ASCII.GetBytes(payload);
+    }
+
+    /// <summary>
+    /// Decodes ASCII <paramref name="bytes"/> into a string.
+    /// </summary>
+    /// <param name="bytes">The bytes to decode.</param>
+    /// <returns>The decoded payload.</returns>
+    public string Decode(ReadOnlySpan<byte> bytes)
+    {
+        return System.Text.Encoding.ASCII.GetString(bytes);
+    }
+}
