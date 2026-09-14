ce10b1fa6a53c442da9b9d11e1b84cffcf0b4a6d
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-14 07:51:44 +0200

    added scan descriptor builder

---
+++ src/dependency-injection/DandyDotnet.DependencyInjection/Scanning/ScanDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DandyDotnet.DependencyInjection.Scanning;
+
+public sealed class ScanDescriptor
+{
+    public required Type AbstractType { get; init; }
+    public ServiceLifetime Lifetime { get; init; } = ServiceLifetime.Transient;
+    public Func<Type, bool>? Predicate { get; init; }
+    public object? Key  { get; init; }
+    public Func<IServiceProvider, object?, object>? KeyedFactory { get; init; }
+    public Func<IServiceProvider, object>? Factory { get; init; }
+    public bool IsOpenGeneric { get; init; }
+}
