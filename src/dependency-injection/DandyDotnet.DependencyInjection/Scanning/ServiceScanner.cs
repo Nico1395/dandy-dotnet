c074147414799cce3a76b123bc6b235b0a61c1a7
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-14 07:52:09 +0200

    added service scanner

---
+++ src/dependency-injection/DandyDotnet.DependencyInjection/Scanning/ServiceScanner.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DandyDotnet.DependencyInjection.Scanning;
+
+public sealed class ServiceScanner(IReadOnlyDictionary<Type, ScanDescriptor> descriptors, Assembly[] assemblies)
+{
+    public IReadOnlyDictionary<Type, ScanDescriptor> Descriptors { get; } = descriptors;
+    public Assembly[] Assemblies { get; } = assemblies;
+
+    public IEnumerable<ServiceDescriptor> GetServiceDescriptors()
+    {
+    }
+}
---
+++ src/dependency-injection/DandyDotnet.DependencyInjection/Scanning/ServiceScannerBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace DandyDotnet.DependencyInjection.Scanning;
+
+public static class ServiceScannerBuilderExtensions
+{
+    public static ServiceScannerBuilder ScanFor<T>(this ServiceScannerBuilder builder, Action<ScanDescriptorBuilder> builderAction)
+    {
+        return builder.ScanFor(typeof(T), builderAction);
+    }
+}
