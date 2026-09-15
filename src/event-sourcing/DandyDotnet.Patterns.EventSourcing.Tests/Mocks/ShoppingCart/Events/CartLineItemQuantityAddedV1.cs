79bc7d5d6f7a320246b94e56490ea675bc543b1a
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-15 23:48:35 +0200

    removed obsolete event sourcing test proj

--- src/event-sourcing/DandyDotnet.Patterns.EventSourcing.Tests/Mocks/ShoppingCart/Events/CartLineItemQuantityAddedV1.cs
+++
@@ -1,9 +0,0 @@
-using DandyDotnet.Patterns.EventSourcing.Abstractions;
-
-namespace DandyDotnet.Patterns.EventSourcing.Tests.Mocks.ShoppingCart.Events;
-
-[Event]
-internal sealed record CartLineItemQuantityAddedV1(
-    Guid CartId,
-    Guid ProductId,
-    int Quantity);
