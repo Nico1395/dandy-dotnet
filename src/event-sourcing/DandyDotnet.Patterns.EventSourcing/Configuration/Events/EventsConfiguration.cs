3abffec9aa5089f54adea06a570e784b4a252d82
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-16 12:37:33 +0200

    added missing lifetime property to EventAttribute

--- src/event-sourcing/DandyDotnet.Patterns.EventSourcing/Configuration/Events/EventsConfiguration.cs
+++ src/event-sourcing/DandyDotnet.Patterns.EventSourcing/Configuration/Events/EventsConfiguration.cs
@@ -23,6 +23,7 @@ public sealed class EventsConfiguration
             {
                 Key = attribute?.Key ?? eventType.Name,
                 RuntimeType = eventType,
+                Lifetime = attribute != null ? TimeSpan.FromMinutes(attribute.LifetimeMinutes) : null,
             };
 
             return EventConfigsByKey[configuration.Key] = configuration;
