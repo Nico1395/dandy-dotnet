70de8ca08c6fcc8c1e399c22c631e126eac90166
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-16 13:00:00 +0200

    filtering for max retries

--- src/event-sourcing/DandyDotnet.Patterns.EventSourcing.Abstractions/SubscriberAttribute.cs
+++ src/event-sourcing/DandyDotnet.Patterns.EventSourcing.Abstractions/SubscriberAttribute.cs
@@ -5,5 +5,5 @@ public sealed class SubscriberAttribute : Attribute
 {
     public SubscriberMode Mode { get; init; } = SubscriberMode.Async;
     public string? Key { get; init; }
-    public int Retries { get; internal set; } = -1;
+    public int Retries { get; init; } = -1;
 }
