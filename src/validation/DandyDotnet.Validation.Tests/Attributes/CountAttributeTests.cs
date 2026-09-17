ca549eb8d5d57fe87d21af9f519af3d3c1eb9254
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-17 22:25:32 +0200

    test classes

---
+++ src/validation/DandyDotnet.Validation.Tests/Attributes/CountAttributeTests.cs
@@ -0,0 +1,19 @@
+namespace DandyDotnet.Validation.Tests.Attributes;
+
+public class CountAttributeTests
+{
+    // Count_EqualCount_ReturnsTrue
+    // Count_NotEqualCount_ReturnsFalse
+    // Count_PrecedesMinOrMax
+    // Count_AllowNullItems_DoesntFilterItems
+    // Count_DontAllowNullItems_FiltersItems
+    // Min_GreaterCount_ReturnsTrue
+    // MinAndEquals_EqualCount_ReturnsTrue
+    // MinAndNotEquals_EqualCount_ReturnsFalse
+    // Min_LowerCount_ReturnsFalse
+    // Max_GreaterCount_ReturnsFalse
+    // MaxAndEquals_EqualCount_ReturnsTrue
+    // MaxAndNotEquals_EqualCount_ReturnsFalse
+    // Max_LowerCount_ReturnsTrue
+    // NotEnumerableValue_ThrowsArgumentException
+}
---
+++ src/validation/DandyDotnet.Validation.Tests/Attributes/NotWhitespaceAttributeTests.cs
@@ -0,0 +1,10 @@
+namespace DandyDotnet.Validation.Tests.Attributes;
+
+public class NotWhitespaceAttributeTests
+{
+    // EmptyValue_ReturnsFalse
+    // WhitespaceValue_ReturnsFalse
+    // NullValue_ReturnsFalse
+    // FilledValue_ReturnsTrue
+    // NotStringValue_ThrowsArgumentException
+}
