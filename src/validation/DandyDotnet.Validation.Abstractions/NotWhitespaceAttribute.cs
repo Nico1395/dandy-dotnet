63fc90fadaab8a682632a61340cdafb679a4c097
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-17 22:25:24 +0200

    not whitespace attribute

---
+++ src/validation/DandyDotnet.Validation.Abstractions/NotWhitespaceAttribute.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DandyDotnet.Validation.Abstractions;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property)]
+public sealed class NotWhitespaceAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not string stringValue)
+            throw new ArgumentException("Value is not a string.", nameof(value));
+
+        return !string.IsNullOrWhiteSpace(stringValue);
+    }
+}
