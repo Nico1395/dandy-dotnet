6f5f913f4d7f90d80f048c54f514c1cab79797ac
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-21 21:43:11 +0200

    added tags column

---
+++ src/event-sourcing/DandyDotnet.Patterns.EventSourcing.Persistence.Sql.SqlServer/Migrations/Migration_20260921212000.cs
@@ -0,0 +1,25 @@
+using DandyDotnet.Patterns.EventSourcing.Persistence.Sql.Constants;
+using DandyDotnet.Persistence.Sql.Migrations.Abstractions;
+
+namespace DandyDotnet.Patterns.EventSourcing.Persistence.Sql.SqlServer.Migrations;
+
+internal sealed class Migration_20260921212000 : IMigration
+{
+    public long Version => 20260921212000;
+
+    public void Up(IMigrationBuilder builder)
+    {
+        builder.Execute($"""
+                         ALTER TABLE [{Sql.Constants.Schema.Name}].[{Tables.Envelopes.Table}]
+                         ADD COLUMN [{Tables.Envelopes.Tags}] NVARCHAR(MAX) NULL;
+                         """);
+    }
+
+    public void Down(IMigrationBuilder builder)
+    {
+        builder.Execute($"""
+                         ALTER TABLE [{Sql.Constants.Schema.Name}].[{Tables.Envelopes.Table}]
+                         DROP COLUMN [{Tables.Envelopes.Tags}];
+                         """);
+    }
+}
